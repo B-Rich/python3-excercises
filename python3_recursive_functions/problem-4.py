1fbf7bd9c9e5eb5af2c6f3041d573e34a5c5301d
Luke Thomas <luk3thomas@gmail.com>
2012-10-21 22:24:31 -0500

    print out fib numbers, not working

--- python3_recursive_functions/problem-4.py
+++ python3_recursive_functions/problem-4.py
@@ -2,12 +2,31 @@
 The Fibonacci numbers are hidden inside of Pascal's triangle. If you sum up the coloured numbers of the following triangle, you will get the 7th Fibonacci number:
 """
 
-def pascal(r, c):
+def printFib(x):
+  for row in range(x):
+    row += 1 # b/c zero index
+    out = 0
+
+    for col in range(row):
+      col += 1 # b/c zero index
+      out += fibViaPascal(row, col)
+
+    print(out)
+
+  return ""
+
+
+def fibViaPascal(r, c):
   """
   print out pascals triangle
   """
-  if(c == 1 or c >= r):
+  if(c < 1 or c > r):
+    return 0
+  
+  if(c == 1 or c == r):
     return 1
   
-  return pascal(r-1, c) + pascal(r-1, c-1)
+  return fibViaPascal(r-1, c-1) + fibViaPascal(r-2, c+2) 
 
+
+printFib(10)
